23711ff99697da16d2e3ee3ab137d3ebf56ce3e3
suhailmalik07 <suhailmalik8057@gmail.com>
2019-07-20 11:31:00 +0530

    added django-heroku

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,4 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # LOGIN_REDIRECT_URL = '/'
+django_heroku.settings(locals())
