a53ad3be9f1ce62c8434294e44fa5ff91a448175
Suhail Malik <suhailmalik8057@gmail.com>
2019-10-30 13:04:46 +0530

    updated settings

--- TaskManager/settings/__init__.py
+++ TaskManager/settings/__init__.py
@@ -2,7 +2,8 @@ import os
 
 # you need to set "myproject = 'prod'" as an environment variable
 # in your OS (on which your website is hosted)
-if os.environ.get('myproject') == 'prod':
+IS_PRODUCTION = os.environ.get('IS_PRODUCTION')
+if IS_PRODUCTION == 'True':
     from .prod import *
 else:
     from .dev import *
